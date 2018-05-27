3c389c040cfe6df17a1641af6e66f298787b0160
Andrii Radyk <andrii.radyk@sizmek.com>
2018-05-27 22:17:02 +0200

    add case for windows build

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,11 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin $CRATE_NAME --target $TARGET --release -- -C lto
+    cross rustc --bin $CRATE_NAME --target $TARGET --release
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/${CRATE_NAME}* $stage/
+    [ -f "target/$TARGET/release/${CRATE_NAME}" ] && cp target/$TARGET/release/${CRATE_NAME} $stage/
+    [ -f "target/$TARGET/release/${CRATE_NAME}.exe" ] && cp target/$TARGET/release/${CRATE_NAME} $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
